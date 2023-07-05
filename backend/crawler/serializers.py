a9f227d51f32f5414c7531a06c31d6d08c4b4899
musong <txz32102@gmail.com>
2023-07-05 05:28:52 -0400

    console.log can show the info from crawl

--- backend/crawler/serializers.py
+++ backend/crawler/serializers.py
@@ -1,11 +1,24 @@
 from rest_framework import serializers
+import requests
 from .models import Crawler
 
 class CrawlerSerializer(serializers.ModelSerializer):
     tags = serializers.SerializerMethodField()
 
     def get_tags(self, instance):
-        return "Hello, world!"
+        url = instance.url
+        keyword = instance.keyword
+
+        # Make a request to the URL
+        response = requests.get(url)
+        content = response.text
+
+        # Search for the keyword in the content
+        if keyword in content:
+            return content
+        else:
+            return "Keyword not found in the URL content."
+
     
     class Meta:
         model = Crawler
