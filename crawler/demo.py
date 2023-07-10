1c5491c9035ab8edd3bffbd4ea41b6a7a93f2893
musong <txz32102@gmail.com>
2023-07-09 23:57:56 -0400

    add the crawl folder for crawling information from www.fao.org

---
+++ crawler/demo.py
@@ -0,0 +1,80 @@
+import requests
+from bs4 import BeautifulSoup
+
+def crawl_links(url):
+    # Send a GET request to the URL
+    response = requests.get(url)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        # Find all the <a> tags (links) in the HTML
+        links = soup.find_all('a')
+        
+        # Create a list to store the extracted links
+        extracted_links = []
+        
+        # Iterate over the links and append their href attribute to the list
+        for link in links:
+            href = link.get('href')
+            extracted_links.append(href)
+        
+        return extracted_links
+    else:
+        print(f"Request failed with status code {response.status_code}")
+        return None
+
+def crawl_sublink(url, index):
+    # Call the crawl_links function with the URL
+    result = crawl_links(url)
+    write_result_to_file(result)
+
+    # Check if the result is not empty and has at least the specified index
+    if result and len(result) >= index:
+        # Extract the link at the specified index from the result
+        sublink = result[index - 1]
+
+        # Create the absolute URL by joining it with the base URL
+        absolute_url = url.rstrip('/') + '/' + sublink.lstrip('/')
+
+        # Send a GET request to the sublink
+        response = requests.get(absolute_url)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the HTML content using BeautifulSoup
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            # Perform further processing with the sublink's content
+            # ...
+
+            # Example: Get the title of the sublink's page
+            title = soup.title.string
+            print(f"Crawled sublink: {absolute_url}")
+            print(f"Title of the sublink: {title}")
+        else:
+            print(f"Request failed with status code {response.status_code}")
+    else:
+        print("No sublink found at the specified index")
+
+
+def write_result_to_file(result):
+    with open("url.txt", "w") as file:
+        for link in result:
+            file.write(link + "\n")
+    print("Result written to url.txt")
+
+
+# Example usage
+url = 'http://www.fao.org'
+index = 2
+
+crawl_sublink(url, index)
+
+# Example usage
+url = 'http://www.fao.org'
+index = 20
+
+crawl_sublink(url, index)
